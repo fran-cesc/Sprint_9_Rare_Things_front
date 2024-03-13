c91790c24a0a23636ee4bee72ca8073d7977fa2c
Cesc <francesc9000@gmail.com>
2024-03-13 07:32:22 +0100

    before migrating to scss

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { UsersService } from '../../services/users.service';
 import { ValidatorsService } from '../../services/validators.service';
@@ -9,16 +14,12 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [
-    CommonModule, ReactiveFormsModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class RegisterComponent {
-
   public isEmailRegistered!: boolean;
 
   activeModal = inject(NgbActiveModal);
@@ -27,61 +28,68 @@ export class RegisterComponent {
   router = inject(Router);
   userForm: FormGroup;
 
-  constructor(){
+  constructor() {
     this.userForm = new FormGroup({
-      user_name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      email: new FormControl('', [Validators.required, Validators.pattern(this.customValidators.emailPattern)]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    })
-
+      user_name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(this.customValidators.emailPattern),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
 
-
-  public isValidField(field: string){
+  public isValidField(field: string) {
     return this.customValidators.isValidField(this.userForm, field);
   }
 
-  public getFieldError(form: FormGroup, field: string){
+  public getFieldError(form: FormGroup, field: string) {
     return this.customValidators.getFieldError(this.userForm, field);
   }
 
-  public async onSubmit(){
-    try{
+  public async onSubmit() {
+    try {
       this.userForm.markAllAsTouched();
-      if (this.userForm.invalid){
+      if (this.userForm.invalid) {
         return;
       }
 
       const userMail = await this.userForm.get('email')!.value;
 
-      this.isEmailRegistered = await this.usersService.isMailRegistered(userMail);
+      this.isEmailRegistered = await this.usersService.isMailRegistered(
+        userMail
+      );
 
       if (this.isEmailRegistered) {
-        alert("email is already registered");
+        alert('email is already registered');
         this.userForm.reset();
         this.activeModal.close();
         return;
       }
-
 
       await this.usersService.register(this.userForm.value);
       const response: any = await this.usersService.login(this.userForm.value);
 
       if (!response.error) {
         localStorage.setItem('token', response.token);
+        this.usersService.currentUser.set(response.results[0]);
         this.userForm.reset();
-        alert("User registered successfuly");
+        alert('User registered successfuly');
         this.activeModal.close();
         this.router.navigate(['/home']);
       }
-    }
-    catch (error){
+    } catch (error) {
       throw error;
     }
   }
 
-  public cancel(){
+  public cancel() {
     this.activeModal.close();
   }
 }
-
