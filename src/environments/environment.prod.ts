9048e98baa39ca00b806b362d9838c46b6d4360a
Cesc <francesc9000@gmail.com>
2024-11-19 12:55:36 +0100

    prod environment updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
-// export const environment = {
-//   BACKEND_BASE_URL: 'https://rare-things-back.onrender.com',
-//   BACKEND_ALL_THINGS_URL: '/things',
-//   HTTP_CODE_SUCCESS: 200,
-//   HTTP_CODE_BAD_REQUEST: 400,
-//   HTTP_CODE_UNAUTHORIZED: 401,
-//   HTTP_CODE_FORBIDDEN: 403
-// }
+export const environment = {
+  BACKEND_BASE_URL: 'https://rare-things-back.onrender.com',
+  BACKEND_ALL_THINGS_URL: '/things',
+  HTTP_CODE_SUCCESS: 200,
+  HTTP_CODE_BAD_REQUEST: 400,
+  HTTP_CODE_UNAUTHORIZED: 401,
+  HTTP_CODE_FORBIDDEN: 403
+}
